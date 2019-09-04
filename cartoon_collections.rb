0c976a9ca463830b527e905af8e72d6e5c02a299
Jennifer Silva <jennifersilva20@gmail.com>
2019-09-04 19:36:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,5 +8,19 @@ end
 def summon_captain_planet(planeteer_calls) # code an argument here
   planeteer_calls.collect do |call|
     call.capitalize << "!"
+  end
 end
 
+def long_planeteer_calls(calls)# code an argument here
+  calls.any? do |call|
+    call.length > 4
+end
+
+def find_the_cheese(array) #code an argument here
+   i = 0
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  while i < cheese_types.length
+    return cheese_types[i] if array.include?(cheese_types[i])
+    i += 1
+  end
+end
